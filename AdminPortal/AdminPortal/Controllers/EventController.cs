93a1ceb1d73ceafff8e122275ae607809197f5fa
codesnchill <siahjayden.17@ichat.sp.edu.sg>
2020-01-23 18:41:35 +0800

    Did multiple selection function for AddEvent department list, pagination and post method

--- AdminPortal/AdminPortal/Controllers/EventController.cs
+++ AdminPortal/AdminPortal/Controllers/EventController.cs
@@ -12,6 +12,7 @@ namespace AdminPortal.Controllers
     {
 
         IEnumerable<Event> events = null;
+        public PaginatedList<Event> eventList { get; private set; }
         public IActionResult ManageEvents()
         {
             using (var client = new HttpClient())
@@ -28,7 +29,10 @@ namespace AdminPortal.Controllers
                     readTask.Wait();
 
                     events = readTask.Result;
-                   
+
+                    eventList = PaginatedList<Event>.Create(events, 1, 4, 5);
+
+                    //HttpContext.Session["employeeList"] = employeeList;
                 }
                 else //web api sent error response 
                 {
@@ -42,7 +46,7 @@ namespace AdminPortal.Controllers
 
             }
 
-            return View(events);
+            return View(eventList);
         }
 
         public IActionResult AddEvent()
@@ -53,6 +57,41 @@ namespace AdminPortal.Controllers
         public IActionResult EditEvent()
         {
             return View();
+        }
+
+        public IActionResult SetPage(string pageIndex)
+        {
+            //int pageIndex = int.Parse(RouteData.Values["pageIndex"].ToString());
+            int pageIn = int.Parse(pageIndex);
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44300/api/v1/");
+                //HTTP GET
+                var responseTask = client.GetAsync("events");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Event>>();
+                    readTask.Wait();
+
+                    events = readTask.Result;
+
+                    eventList = PaginatedList<Event>.Create(events, pageIn, 4, 5);
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    events = Enumerable.Empty<Event>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+
+
+            }
+            return View("ManageEvents", eventList);
         }
     }
 }
