b4b52797f0c43e553308d3850ba4626eff64783f
jafflim <limhj14.17@ichat.sp.edu.sg>
2020-01-28 14:08:35 +0800

    working on report

--- AdminPortal/AdminPortal/Controllers/ReportController.cs
+++ AdminPortal/AdminPortal/Controllers/ReportController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
 
 namespace AdminPortal.Controllers
 {
@@ -11,6 +13,49 @@ namespace AdminPortal.Controllers
     {
         public IActionResult GenerateReport()
         {
+            return View();
+        }
+
+        public IActionResult AuditReport()
+        {
+            //HtmlToPdfConverter converter = new HtmlToPdfConverter();
+
+            //WebKitConverterSettings settings = new WebKitConverterSettings();
+            //settings.WebKitPath = ("C:/AdminPortal/AdminPortal/AdminPortal/QtBinariesWindows/");
+            //converter.ConverterSettings = settings;
+
+            //PdfDocument document = converter.Convert("https://localhost:44332/Point/ManagePoints");
+
+            //MemoryStream ms = new MemoryStream();
+            //document.Save(ms);
+            //document.Close(true);
+            //ms.Position = 0;
+
+            //FileStreamResult fsr = new FileStreamResult(ms, "application/pdf");
+            //fsr.FileDownloadName = "invoice.pdf";
+
+            //return fsr;
+            return View();
+        }
+        public IActionResult MilestoneReport()
+        {
+            //HtmlToPdfConverter converter = new HtmlToPdfConverter();
+
+            //WebKitConverterSettings settings = new WebKitConverterSettings();
+            //settings.WebKitPath = ("C:/AdminPortal/AdminPortal/AdminPortal/QtBinariesWindows/");
+            //converter.ConverterSettings = settings;
+
+            //PdfDocument document = converter.Convert("https://localhost:44332/Point/ManagePoints");
+
+            //MemoryStream ms = new MemoryStream();
+            //document.Save(ms);
+            //document.Close(true);
+            //ms.Position = 0;
+
+            //FileStreamResult fsr = new FileStreamResult(ms, "application/pdf");
+            //fsr.FileDownloadName = "invoice.pdf";
+
+            //return fsr;
             return View();
         }
     }
