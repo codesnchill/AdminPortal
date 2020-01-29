5f613e45a0bafe6fa316ebcd808a2e67c072c276
codesnchill <siahjayden.17@ichat.sp.edu.sg>
2020-01-29 21:19:08 +0800

    Added Milestone implementations for Add, Edit & Delete Milestone

---
+++ AdminPortal/AdminPortal/Models/Milestone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPortal.Models
+{
+    public class Milestone
+    {
+
+        public int RewardId { get; set; }
+        public string Reward { get; set; }
+        public int Points { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public string DeletedBy { get; set; }
+    }
+}
