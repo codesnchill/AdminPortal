ecaa318620013e9c6c1e505303c783f485d48c1f
lokidoki102 <lokidoki102@users.noreply.github.com>
2020-01-30 17:34:54 +0800

    Added filter to manage users page search

--- AdminPortal/AdminPortal/Controllers/UserController.cs
+++ AdminPortal/AdminPortal/Controllers/UserController.cs
@@ -150,10 +150,95 @@ namespace AdminPortal.Controllers
             return View();
         }
 
-        public IActionResult Search()
+        [HttpGet]
+        public JsonResult Search([FromQuery] EmployeeSearchQuery employeeQuery)
         {
+            List<dynamic> employeeList = new List<dynamic>();
 
-            return View("ManageUser");
+            if (string.IsNullOrWhiteSpace(HttpContext.Session.GetString(EmployeeListSessionName)))
+            {
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44300/api/v1/");
+                    //HTTP GET
+                    var responseTask = client.GetAsync("users");
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<IList<Employee>>();
+                        readTask.Wait();
+
+                        employee = readTask.Result;
+                        employee = employee.ToList();
+
+                        // store employee in session
+                        HttpContext.Session.SetComplexData(EmployeeListSessionName, employee);
+
+                        // do filter
+                        if (!string.IsNullOrWhiteSpace(employeeQuery.EmployeeName))
+                            employee = employee.Where(u => u.EmployeeName.Contains(employeeQuery.EmployeeName));
+                        if (!string.IsNullOrWhiteSpace(employeeQuery.EmployeeId))
+                            employee = employee.Where(u => u.EmployeeName.Contains(employeeQuery.EmployeeId));
+                        if (!string.IsNullOrWhiteSpace(employeeQuery.DepartmentName))
+                            employee = employee.Where(u => u.DepartmentName.Equals(employeeQuery.DepartmentName));
+                        if (!string.IsNullOrWhiteSpace(employeeQuery.Roles))
+                            employee = employee.Where(u => u.Roles.Equals(employeeQuery.Roles));
+                    }
+                    else //web api sent error response 
+                    {
+                        //log response status here..
+
+                        employee = Enumerable.Empty<Employee>();
+
+                        ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                    }
+
+                }
+
+            }
+            else
+            {
+                employee = HttpContext.Session.GetComplexData<IEnumerable<Employee>>(EmployeeListSessionName);
+
+                // do filter
+                if (!string.IsNullOrWhiteSpace(employeeQuery.EmployeeName))
+                    employee = employee.Where(u => u.EmployeeName.Contains(employeeQuery.EmployeeName));
+                if (!string.IsNullOrWhiteSpace(employeeQuery.EmployeeId))
+                    employee = employee.Where(u => u.EmployeeId.Contains(employeeQuery.EmployeeId));
+                if (!string.IsNullOrWhiteSpace(employeeQuery.DepartmentName))
+                    employee = employee.Where(u => u.DepartmentName.Equals(employeeQuery.DepartmentName));
+                if (!string.IsNullOrWhiteSpace(employeeQuery.Roles))
+                    employee = employee.Where(u => u.Roles.Equals(employeeQuery.Roles));
+            }
+
+            var employeeResult = employee.Select(employee => new
+            {
+                DepartmentName = employee.DepartmentName,
+                EmployeeName = employee.EmployeeName,
+                EmployeeBio = employee.EmployeeBio,
+                Roles = employee.Roles,
+                EmployeeId = employee.EmployeeId,
+                IsDisabled = employee.IsDisabled,
+                totalPoints = employee.totalPoints
+            });
+
+            foreach (var oneResult in employeeResult)
+            {
+                employeeList.Add(new
+                {
+                    departmentName = oneResult.DepartmentName,
+                    employeeName = oneResult.EmployeeName,
+                    employeeBio = oneResult.EmployeeBio,
+                    roles = oneResult.Roles,
+                    employeeId = oneResult.EmployeeId,
+                    isDisabled = oneResult.IsDisabled,
+                    totalPoints = oneResult.totalPoints
+                });
+            }
+            return new JsonResult(employeeList);
         }
 
 
---
+++ AdminPortal/AdminPortal/Models/EmployeeSearchQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPortal.Models
+{
+    public class EmployeeSearchQuery
+    {
+        public string EmployeeId { get; set; }
+        public string DepartmentName { get; set; }
+        public string Roles { get; set; }
+        public string IsDisabled { get; set; }
+        public string DepartmentId { get; set; }
+        public string EmployeeName { get; set; }
+    }
+}
