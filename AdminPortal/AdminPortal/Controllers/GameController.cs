e93925faf9d2e4f0a555d75c98ba0cbcb08371a5
codesnchill <siahjayden.17@ichat.sp.edu.sg>
2020-02-03 17:42:08 +0800

    Modified ManageGames to make dropdown list on change work to append datatable but unsuccessful

--- AdminPortal/AdminPortal/Controllers/GameController.cs
+++ AdminPortal/AdminPortal/Controllers/GameController.cs
@@ -11,7 +11,7 @@ namespace AdminPortal.Controllers
     public class GameController : Controller
     {
         IEnumerable<Question> questions = null;
-        const string QuestionListSessionName = "_GameQuestionList";
+        const string QuestionListSessionName = "_CompanyQuizQuestionList";
         public IActionResult ManageGames()
         {
             List<Question> questionList = new List<Question>();
@@ -49,6 +49,93 @@ namespace AdminPortal.Controllers
 
             //make sure it returns the whole list retrieve from database (not the paginated list)
             return View(questionList);
+        }
+
+        IEnumerable<Question> questions2 = null;
+        const string QuestionListSessionName2 = "_GuessTheEmployeeQuestionList";
+
+
+        [HttpGet]
+        public JsonResult LoadCompanyQuiz()
+        {
+            List<Question> questionList = new List<Question>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44300/api/v1/");
+                //HTTP GET
+                var responseTask = client.GetAsync("games/2?deleted=false");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Question>>();
+                    readTask.Wait();
+
+                    questions2 = readTask.Result;
+                    questionList = questions2.ToList();
+
+                    // store employee in session
+                    HttpContext.Session.SetComplexData(QuestionListSessionName2, questionList);
+                    //HttpContext.Session["employeeList"] = employeeList;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    questions2 = Enumerable.Empty<Question>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+
+
+            }
+
+            //make sure it returns the whole list retrieve from database (not the paginated list)
+            return new JsonResult(questionList);
+        }
+
+
+
+        IEnumerable<Question> questions1 = null;
+        [HttpGet]
+        public IActionResult LoadGuessTheEmployeeQuiz()
+        {
+            List<Question> questionList = new List<Question>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44300/api/v1/");
+                //HTTP GET
+                var responseTask = client.GetAsync("games/1?deleted=false");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Question>>();
+                    readTask.Wait();
+
+                    questions1 = readTask.Result;
+                    questionList = questions1.ToList();
+
+                    // store employee in session
+                    HttpContext.Session.SetComplexData(QuestionListSessionName, questionList);
+                    //HttpContext.Session["employeeList"] = employeeList;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    questions1 = Enumerable.Empty<Question>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+
+
+            }
+
+            //make sure it returns the whole list retrieve from database (not the paginated list)
+            return View("ManageGames", questionList);
         }
 
         public IActionResult EditQuestion()
