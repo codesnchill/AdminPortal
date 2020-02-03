2e0aaa4f5d4d7520eac46ecaa1169922c875f4f8
jafflim <limhj14.17@ichat.sp.edu.sg>
2020-02-03 23:50:03 +0800

    Complete all the generate Report

--- AdminPortal/AdminPortal/Controllers/ReportController.cs
+++ AdminPortal/AdminPortal/Controllers/ReportController.cs
@@ -16,29 +16,24 @@ namespace AdminPortal.Controllers
     public class ReportController : Controller
     {
         IEnumerable<Milestone> report = null;
+        IEnumerable<RankingReport> rankingreport = null;
+
         IEnumerable<AuditReport> auditreport = null;
         public PaginatedList<Milestone> reportList { get; private set; }
         public PaginatedList<AuditReport> auditreportList { get; private set; }
+        public PaginatedList<RankingReport> rankingreportList { get; private set; }
         public IActionResult GenerateReport()
         {
             return View();
         }
 
-        public IActionResult AuditReport()
-        {
-
-            return new ViewAsPdf();
-        }
-
         [HttpGet]
-        public IActionResult AuditReport([FromQuery] AuditReport employeeQuery)
+        public IActionResult AuditReport(string ReportType, string StartDate, string EndDate)
         {
-            //var type = employeeQuery.ReportType;
-            //var start = employeeQuery.StartDate;
-            //var end = employeeQuery.EndDate;
-            var type = "audit";
-            var start = "2020-01-01";
-            var end = "2020-01-31";
+            var type = ReportType;
+            var start = StartDate;
+            var end = EndDate;
+
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://localhost:44300/api/v1/");
@@ -68,21 +63,20 @@ namespace AdminPortal.Controllers
             }
         }
 
-
         [HttpGet]
-        public IActionResult MilestoneReport([FromQuery] Report employeeQuery)
+        public IActionResult MilestoneReport(string ReportType, string StartDate, string EndDate)
         {
             //var type = employeeQuery.ReportType;
             //var start = employeeQuery.StartDate;
             //var end = employeeQuery.EndDate;
-            var type = "milestone";
-            var start = "2020-01-01";
-            var end = "2020-01-31";
+            var type = ReportType;
+            var start = StartDate;
+            var end = EndDate;
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://localhost:44300/api/v1/");
                 //HTTP GET
-                var responseTask = client.GetAsync("report?ReportType="+type+"&StartDate="+start+"&EndDate="+end);
+                var responseTask = client.GetAsync("report?ReportType=" + type + "&StartDate=" + start + "&EndDate=" + end);
                 responseTask.Wait();
 
                 var result = responseTask.Result;
@@ -102,11 +96,53 @@ namespace AdminPortal.Controllers
 
                     ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
                 }
-                return View(reportList);
+
+                return new ViewAsPdf(reportList);
 
             }
+            //    return View();
+            //}      
+
         }
+        [HttpGet]
+        public IActionResult RankingReport(string ReportType, string StartDate, string EndDate)
+        {
+            //var type = employeeQuery.ReportType;
+            //var start = employeeQuery.StartDate;
+            //var end = employeeQuery.EndDate;
+            var type = ReportType;
+            var start = StartDate;
+            var end = EndDate;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44300/api/v1/");
+                //HTTP GET
+                var responseTask = client.GetAsync("report?ReportType=" + type + "&StartDate=" + start + "&EndDate=" + end);
+                responseTask.Wait();
 
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<RankingReport>>();
+                    readTask.Wait();
 
-}
+                    rankingreport = readTask.Result;
+                    rankingreportList = PaginatedList<RankingReport>.Create(rankingreport, 1, 4, 5);
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    rankingreport = Enumerable.Empty<RankingReport>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+                return new ViewAsPdf(rankingreportList);
+
+            }
+            //    return View();
+            //}      
+
+        }
+    }
 }
---
+++ AdminPortal/AdminPortal/Models/RankingReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPortal.Models
+{
+    public class RankingReport
+    {
+        public string DepartmentName { get; set; }
+        public int Placement { get; set; }
+        public double AvgPoint { get; set; }
+
+    }
+}
