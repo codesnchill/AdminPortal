055833d90dc8b417d3d3589dea7a15bb2e2df2c3
codesnchill <siahjayden.17@ichat.sp.edu.sg>
2020-02-04 16:48:14 +0800

    Changed the start up page

--- AdminPortal/AdminPortal/Startup.cs
+++ AdminPortal/AdminPortal/Startup.cs
@@ -64,7 +64,7 @@ namespace AdminPortal
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
             });
             RotativaConfiguration.Setup(env2);
         }
